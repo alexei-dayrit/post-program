d253296f0d8cc06490a983f19d05af59ffbf5dfc
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-05-20 00:24:34 +0000

    Added starter files.

---
+++ react-stopwatch/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Stopwatch from './stopwatch';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(
+  <Stopwatch />
+);
---
+++ react-stopwatch/src/stopwatch.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export default function Stopwatch() {
+  return (
+    <>
+      <h1>Stopwatch Component</h1>
+    </>
+  );
+}
