f6bcca7ccfe910719e534327ea0fd440cdded2de
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-05-22 20:00:19 +0000

    Created stopwatch with React using useState hook.

--- react-stopwatch/src/stopwatch.jsx
+++ react-stopwatch/src/stopwatch.jsx
@@ -1,9 +1,42 @@
 import React, { useState } from 'react';
 
 export default function Stopwatch() {
+  const [count, setCount] = useState(0);
+  const [isCounting, setIsCounting] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
+
+  const handleStopwatchButton = () => {
+    if (!isCounting) {
+      setIsCounting(true);
+      setIntervalId(setInterval(() => {
+        tick();
+      }, 1000));
+    } else {
+      setIsCounting(false);
+      clearInterval(intervalId);
+    }
+  };
+
+  const tick = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const reset = () => {
+    if (!isCounting) {
+      setCount(0);
+    }
+  };
+
   return (
-    <>
-      <h1>Stopwatch Component</h1>
-    </>
+    <div className='container'>
+      <h1 className='col-100'>Stopwatch Component</h1>
+      <div className={`col-100 stopwatch ${!isCounting && ('clickable-face')}`} onClick={reset}>
+        {count}
+      </div>
+      <i
+        className={`fa-solid fa-${!isCounting ? 'play' : 'pause'}`}
+        onClick={handleStopwatchButton}
+      ></i>
+    </div>
   );
 }
