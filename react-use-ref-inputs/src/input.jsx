3cdd4832026effc3a9d9371b2475c1798b93aa6f
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-05-30 23:16:43 +0000

    Created focus button for input and persist value through renders without causing rerender.

--- react-use-ref-inputs/src/input.jsx
+++ react-use-ref-inputs/src/input.jsx
@@ -1,12 +1,29 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Input() {
   const [name, setName] = useState('');
+  const inputRef = useRef();
+  const prevName = useRef();
+
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
+
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
 
   return (
     <>
-      <input type="text" value={name} onChange={event => setName(event.target.value)}/>
-      <h1>Hello, my name is... {name}</h1>
+      <input
+        type="text"
+        ref={inputRef}
+        value={name}
+        onChange={event => setName(event.target.value)}
+      />
+      <h1>Hello, my name is &#34;{name}&#34;</h1>
+      <h3>But it used to be &#34;{prevName.current}&#34;</h3>
+      <button onClick={focusInput}>Focus Input</button>
     </>
   );
 }
