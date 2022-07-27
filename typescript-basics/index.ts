45db0461c77511d374fced2792467d7f67dac5ae
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-07-27 16:47:56 -0700

    Added JavaScript sample code.

--- typescript-basics/index.ts
+++ typescript-basics/index.ts
@@ -1,8 +1,19 @@
-const firstName: string = 'Alexei';
-const myAge: number = 24;
+// JavaScript
+const firstName1 = 'Alexei';
+const myAge1 = 24;
 
-const increment = (counterVal: number) => {
+const increment1 = counterVal => {
   return counterVal + 1;
 }
 
-const counter: number = increment(10)
+const counter1: number = increment1(10)
+
+// TypeScript
+const firstName2: string = 'Alexei';
+const myAge2: number = 24;
+
+const increment2 = (counterVal: number) => {
+  return counterVal + 1;
+}
+
+const counter2: number = increment2(10)
