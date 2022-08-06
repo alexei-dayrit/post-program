cee0e4792b2136e023b917396bb2b9dbeb0fee0d
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-08-05 18:58:15 -0700

    Added a gaurd for /products route to not accept numbers past 2.

--- react-router/src/pages/Product.jsx
+++ react-router/src/pages/Product.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom'
+import NotFound from './NotFound';
 
 const Product = () => {
-  const { id } = useParams()
+  let { id } = useParams()
+  id = Number(id)
+  if (id < 3) {
+    return (
+      <h1 style={{ color: 'green' }}>
+        Product {id}
+      </h1>
+    )
+  }
 
   return (
-    <h1 style={{color: 'green'}}>
-      Product {id}
-    </h1>
+    <NotFound />
   )
 }
 
