c4ad3f908ce26c998d282c38221d2754063c91b6
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-08-05 17:22:32 -0700

    Created navbar component and imported it into App component.

--- react-router-basics/src/App.jsx
+++ react-router-basics/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Nav'
 import Home from './pages/Home'
+import Products from './pages/Products';
 
 const App = () => {
   return (
-    <div className='App'>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-      </Routes>
+    <div>
+      <Navbar />
+      <div className='app-pages'>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/products' element={<Products />}/>
+        </Routes>
+      </div>
     </div>
   );
 }
