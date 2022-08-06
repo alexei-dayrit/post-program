dbe19dafef3543a4ba97065fa590455f9d57cab5
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-08-05 17:14:00 -0700

    Created default path to route to Home component.

--- react-router-basics/src/App.jsx
+++ react-router-basics/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <>
+    <div className='App'>
       <Routes>
-        <Route />
-        <Route />
+        <Route path='/' element={<Home />}/>
       </Routes>
-    </>
+    </div>
   );
 }
 
