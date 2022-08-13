1675816ad0b589085ca62ec27217439f8f3f96c9
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-08-13 13:51:11 -0700

    Fetched products data with useEffect.

--- react-search-bar/src/App.jsx
+++ react-search-bar/src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 import './styles.css';
 import SearchBar from './components/SearchBar';
 
 function App() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        const data = await response.json();
+        await setProducts(data.products);
+      } catch(err) {
+        console.errror(err);
+      }
+    }
+    fetchProducts()
+  }, [])
 
   return (
     <div>
