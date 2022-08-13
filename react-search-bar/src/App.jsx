3fc94451cd00a078f130e1868d6ee4ee4125eaf4
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-08-13 14:49:07 -0700

    mapped through products state to display image/title. Added styling to products as well.

--- react-search-bar/src/App.jsx
+++ react-search-bar/src/App.jsx
@@ -1,29 +1,14 @@
-import { useState, useEffect } from 'react';
 import './styles.css';
 import SearchBar from './components/SearchBar';
 import Product from './components/Product';
 
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch('https://dummyjson.com/products');
-        const data = await response.json();
-        await setProducts(data.products);
-      } catch(err) {
-        console.errror(err);
-      }
-    }
-    fetchProducts()
-  }, [])
 
   return (
     <div>
       <h1 className="header">Search for products!</h1>
       <SearchBar />
-      <Product products={products}/>
+      <Product />
     </div>
   );
 }
--- react-search-bar/src/components/Product.jsx
+++ react-search-bar/src/components/Product.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import '../styles.css'
 
-const Product = ({ products }) => {
+const Product = () => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products?limit=100');
+        const data = await response.json();
+        await setProducts(data.products);
+      } catch (err) {
+        console.errror(err);
+      }
+    }
+    fetchProducts()
+  }, [])
+
   return (
-    <div>Product</div>
+    <div className='container'>
+      <ul className='product-list'>
+        {products.map(product => (
+          <li key={product.id} className='product-item'>
+            <img src={product.images[0]} alt='Product Item'/>
+            <h1>{product.title}</h1>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
