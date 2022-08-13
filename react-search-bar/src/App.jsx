8c2bf2595be851d38effdae764a72686d2599f4c
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-08-13 13:19:13 -0700

    added useState hook for products.

--- react-search-bar/src/App.jsx
+++ react-search-bar/src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import './styles.css';
 import SearchBar from './components/SearchBar';
 
 function App() {
+  const [products, setProducts] = useState([])
+
   return (
     <div>
       <h1 className="header">Search for products!</h1>
