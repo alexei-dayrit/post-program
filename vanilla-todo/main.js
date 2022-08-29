6a76ab28c388da8c1f511ee65abb3edb834211b9
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-08-28 21:15:01 -0700

    Added data model.

--- vanilla-todo/main.js
+++ vanilla-todo/main.js
@@ -2,21 +2,32 @@ const form = document.querySelector('form');
 const todoInput = document.querySelector('#todo-input');
 const todoList = document.querySelector('.todo-list');
 
+const data = {
+  todos: []
+};
+
 form.addEventListener('submit', event => {
   event.preventDefault();
   const todoListElem = document.createElement('li');
   // const newTodo = todoInput.value
-  const newTodo = form.elements.todoInput.value;
-  todoListElem.textContent = newTodo;
+  const newTodo = {
+    note: form.elements.todoInput.value,
+    isCompleted: false
+  };
+  todoListElem.textContent = newTodo.note;
+  data.todos.unshift(newTodo);
 
   todoListElem.addEventListener('click', event => {
     if (event.target.tagName !== 'LI') {
       return;
     }
+
     if (todoListElem.style.textDecoration === '') {
       todoListElem.style.textDecoration = 'line-through';
+      newTodo.isCompleted = true;
     } else {
       todoListElem.style.textDecoration = '';
+      newTodo.isCompleted = false;
     }
   });
 
