b236f4701db08bc0f500cba6f623be93ac3570a0
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-08-28 20:50:47 -0700

    Added cross out feature by adding event listener to list element.

--- vanilla-todo/main.js
+++ vanilla-todo/main.js
@@ -8,6 +8,18 @@ form.addEventListener('submit', event => {
   // const newTodo = todoInput.value
   const newTodo = form.elements.todoInput.value;
   todoListElem.textContent = newTodo;
+
+  todoListElem.addEventListener('click', event => {
+    if (event.target.tagName !== 'LI') {
+      return;
+    }
+    if (todoListElem.style.textDecoration === '') {
+      todoListElem.style.textDecoration = 'line-through';
+    } else {
+      todoListElem.style.textDecoration = '';
+    }
+  });
+
   todoInput.value = '';
   todoList.append(todoListElem);
 });
