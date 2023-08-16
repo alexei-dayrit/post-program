f6f3c38fdf4da271db210dc09dc3ff46e464d389
Alexei Dayrit <alexeigdayrit@gmail.com>
2023-08-15 19:13:57 -0700

    Installed vite app

---
+++ react-todo-list/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+/*
+  INSTRUCTIONS:
+  Create a "todo"(add cities) app with the following criteria.
+    1. The user can add new cities
+    2. The user can remove existing cities items
+*/
+
+export default function App() {
+  const addCities = () => {
+    //Complete function
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
