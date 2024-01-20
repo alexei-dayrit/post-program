94a35cecc9bc433a8c3df6e3ad2f892f3960ed98
Alexei Dayrit <alexeigdayrit@gmail.com>
2024-01-19 21:07:45 -0800

    Added simple input validation via frontend.

--- react-progress-bar/src/ProgressBar.jsx
+++ react-progress-bar/src/ProgressBar.jsx
@@ -2,9 +2,13 @@ const ProgressBar = ({ width }) => {
   return (
     <div>
       <div className="container">
-        <div className="innerContainer" style={{ width: `${width}%` }}>
-          {width}%
-        </div>
+        {width >= 0 && width <= 100 ? (
+          <div className="innerContainer" style={{ width: `${width}%` }}>
+            {width}%
+          </div>
+        ) : (
+          alert("please enter value less than 100")
+        )}
       </div>
     </div>
   )
