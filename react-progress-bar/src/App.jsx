257cc9560b79bce499ab490fb90fb30e0e37116d
Alexei Dayrit <alexeigdayrit@gmail.com>
2024-01-19 20:48:50 -0800

    Added divs to format with correspondingstyle.

--- react-progress-bar/src/App.jsx
+++ react-progress-bar/src/App.jsx
@@ -2,15 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [widthValue, setWidthValue] = useState(0);
+
   const setValuer = () => "";
 
+  const progressBarStyle = {
+
+  };
+
+  const emptyProgressBarStyle = {
+
+  };
 
   return (
     <>
       <div className="app">
         <h1>Progress bar</h1>
-        {/* <ProgressBar width={val} /> */}
         <form>
+          <div style={emptyProgressBarStyle}>
+            <div style={progressBarStyle}>
+              {widthValue}%
+            </div>
+          </div>
           <label htmlFor="html">Input Percentage:</label>
           <input type="number" onChange={setValuer} />
         </form>
