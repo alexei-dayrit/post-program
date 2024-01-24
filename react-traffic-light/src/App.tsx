aa6d86eba91f071a8e284ae74f2f9ca0170687e3
Alexei Dayrit <alexeigdayrit@gmail.com>
2024-01-23 21:46:19 -0800

    Added useEffect to create 3 setTimeouts to handle the light logic.

--- react-traffic-light/src/App.tsx
+++ react-traffic-light/src/App.tsx
@@ -1,8 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [lightState, setLightState] = useState('stop');
+
+  useEffect(() => {
+    const stopTimer = setTimeout(() => {
+      if (lightState === 'stop') {
+        setLightState('slow');
+      }
+    }, 5000);
+
+    const slowTimer = setTimeout(() => {
+      if (lightState === 'slow') {
+        setLightState('go');
+      }
+    }, 3000);
+
+    const goTimer = setTimeout(() => {
+      if (lightState === 'go') {
+        setLightState('stop');
+      }
+    }, 5000);
+
+    return () => {
+      clearTimeout(stopTimer), clearTimeout(slowTimer), clearTimeout(goTimer);
+    };
+  });
 
   return (
       <div className="App">
