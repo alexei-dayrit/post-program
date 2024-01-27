6c2a3bc48a501d9ca101b7148ddedbad0beebbd8
Alexei Dayrit <alexeigdayrit@gmail.com>
2024-01-26 20:38:20 -0800

    Created StopLight component to make it reusable.

--- react-traffic-light/src/App.tsx
+++ react-traffic-light/src/App.tsx
@@ -1,49 +1,11 @@
-import { useState, useEffect } from 'react';
 import './App.css';
-
-type StopLightState = 'stop' | 'slow' | 'go'
-
-const STOP_AND_GO_DELAY = 5000;
-const SLOW_DELAY = 2000;
+import StopLight from './StopLight';
 
 function App() {
-  const [lightState, setLightState] = useState<StopLightState>('stop');
-
-  useEffect(() => {
-    const stopTimer = setTimeout(() => {
-      if (lightState === 'stop') {
-        setLightState('slow');
-      }
-    }, STOP_AND_GO_DELAY);
-
-    const slowTimer = setTimeout(() => {
-      if (lightState === 'slow') {
-        setLightState('go');
-      }
-    }, SLOW_DELAY);
-
-    const goTimer = setTimeout(() => {
-      if (lightState === 'go') {
-        setLightState('stop');
-      }
-    }, STOP_AND_GO_DELAY);
-
-    return () => {
-      clearTimeout(stopTimer), clearTimeout(slowTimer), clearTimeout(goTimer);
-    };
-  });
-
-  function getLightClass(light: StopLightState) {
-    return `light ${light} ${lightState === light ? 'on' : ''}`;
-  }
 
   return (
-    <div className="App">
-      <div className="traffic-light">
-        <div className={getLightClass('stop')}></div>
-        <div className={getLightClass('slow')}></div>
-        <div className={getLightClass('go')}></div>
-      </div>
+    <div>
+      <StopLight />
     </div>
   );
 }
---
+++ react-traffic-light/src/StopLight.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+type StopLightState = 'stop' | 'slow' | 'go';
+
+const STOP_AND_GO_DELAY = 5000;
+const SLOW_DELAY = 2000;
+
+const StopLight = () => {
+  const [lightState, setLightState] = useState<StopLightState>('stop');
+
+  useEffect(() => {
+    const stopTimer = setTimeout(() => {
+      if (lightState === 'stop') {
+        setLightState('slow');
+      }
+    }, STOP_AND_GO_DELAY);
+
+    const slowTimer = setTimeout(() => {
+      if (lightState === 'slow') {
+        setLightState('go');
+      }
+    }, SLOW_DELAY);
+
+    const goTimer = setTimeout(() => {
+      if (lightState === 'go') {
+        setLightState('stop');
+      }
+    }, STOP_AND_GO_DELAY);
+
+    return () => {
+      clearTimeout(stopTimer), clearTimeout(slowTimer), clearTimeout(goTimer);
+    };
+  });
+
+  function getLightClass(light: StopLightState) {
+    return `light ${light} ${lightState === light ? 'on' : ''}`;
+  }
+
+  return (
+    <div className="App">
+      <div className="traffic-light">
+        <div className={getLightClass('stop')}></div>
+        <div className={getLightClass('slow')}></div>
+        <div className={getLightClass('go')}></div>
+      </div>
+    </div>
+  );
+};
+
+export default StopLight;
