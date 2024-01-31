c36670897f972a7ce5a37f3f8b99072700432956
Alexei Dayrit <alexeigdayrit@gmail.com>
2024-01-31 08:59:36 -0800

    Made it so that the nested object is destructured and then the data is displayed on buttons in random order.

--- react-match-country-and-capital/src/App.jsx
+++ react-match-country-and-capital/src/App.jsx
@@ -1,13 +1,51 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+// sample data = {{ Germany: "Berlin", Azerbaijan: "Baku" }}
 
+// destructuring data to make it simpler to work with
+function CountryCapitalGame({ data }) {
+  const [buttonColor, setButtonColor] = useState('')
+
+  const countries = Object.keys(data)
+  const capitals = Object.values(data);
+  const options = [...countries, ...capitals]
+  // randomize button order
+  const randomizedOptions = options.sort(() => Math.random() - 0.5)
+
+  function handleButtonClick() {
+
+    setButtonColor('blue')
+  }
+
+  // using map to remove repetitive code
+  return (<>
+    {/* <div>{JSON.stringify(pairs)}</div> */}
+    <div>
+      {randomizedOptions.map((option) => (
+        <button
+          onClick={() => handleButtonClick()}
+          key={option}
+          style={{ backgroundColor: buttonColor }}
+        >
+          {option}
+        </button>
+
+
+      ))}
+    </div>
+
+  </>);
+}
+
+
+function App() {
   return (
     <>
-
+      <CountryCapitalGame data={{ Germany: "Berlin", Azerbaijan: "Baku" }} />
     </>
   )
 }
+
 
 export default App
