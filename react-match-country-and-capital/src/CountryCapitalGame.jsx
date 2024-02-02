32c4f9e4cc96d16aac931731e0a9e1bd52ef4414
Alexei Dayrit <alexeigdayrit@gmail.com>
2024-02-02 01:00:12 -0800

    Moved function into a separate component.

---
+++ react-match-country-and-capital/src/CountryCapitalGame.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+
+// sample data = {{ Germany: "Berlin", Azerbaijan: "Baku" }}
+
+// destructuring data to make it simpler to work with
+export default function CountryCapitalGame({ data }) {
+  console.log('data:', data)
+  const countries = Object.keys(data)
+  const capitals = Object.values(data);
+  // randomize button order
+  const [options, setOptions] = useState(
+    [...countries, ...capitals].sort(() => Math.random() - 0.5).map((value) => ({ value, state: 'DEFAULT' }))
+  )
+  console.log('options:', options)
+  const [selected, setSelected] = useState('')
+  console.log('selected:', selected)
+
+  const isGameOver = options.length === 0;
+
+  function handleButtonClick(clickedOption) {
+    if (!selected) {
+      setSelected(clickedOption)
+      setOptions(
+        options.map((option) => {
+          return option === clickedOption ? { ...option, state: "SELECTED" } : { ...option, state: 'DEFAULT' };
+        })
+      )
+    } else {
+      // selected correct
+      if (selected.value === data[clickedOption.value] || data[selected.value] === clickedOption.value) {
+        setOptions(options.filter((option) => {
+          return !(option.value === selected.value || option.value === clickedOption.value)
+        }))
+      } else {
+        // selected wrong
+        setOptions(options.map((option) => {
+          return (option.value === selected.value || option.value === clickedOption.value)
+            ? { ...option, state: 'WRONG' } : option
+        }))
+      }
+      setSelected(undefined)
+    }
+  }
+
+  // using map to remove repetitive code
+  return (<>
+    <div>
+      {isGameOver ? (<div>Congratulations</div>)
+        : (options.map((option) => {
+          console.log('option in render map:', option);
+          return (
+            <button
+              onClick={() => handleButtonClick(option)}
+              key={option.value}
+              className={option.state === 'SELECTED' ? 'selected' : option.state === 'WRONG' ? 'wrong' : ''}
+            >
+              {option.value}
+            </button>
+          );
+        }))}
+    </div>
+  </>);
+}
